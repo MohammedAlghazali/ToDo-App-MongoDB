576eb44a17e4f3524dd31a3b6563c8eae881321c
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-10-01 03:22:34 +0300

    remove statusCode in response message in authorization function

--- server/controllers/auth/authorization.js
+++ server/controllers/auth/authorization.js
@@ -4,7 +4,7 @@ const authorization = async (req, res, next) => {
   if (req.cookies && req.cookies.token) {
     verify(req.cookies.token, process.env.SECRET_KEY, (err, payload) => {
       if (err) {
-        res.status(401).json({ statusCode: 401, message: 'Un-Authorized' });
+        res.status(401).json({ message: 'Un-Authorized' });
       } else {
         req.user = payload;
         next();
