eaa8ab5c90263aff8e28b9e0c941253f3424702a
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-10-11 19:12:18 +0300

    make logout route and its function #23

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { signUp, login, authorization, modifyUser } = require('../controllers');
+const {
+  signUp,
+  login,
+  authorization,
+  modifyUser,
+  logout,
+} = require('../controllers');
 
 router.post('/signUp', signUp);
 router.post('/login', login);
 
 router.use(authorization);
 
+router.post('/logout', logout);
 router.get('/is-auth', (req, res) => {
   res.send({ statusCode: 200 });
 });
